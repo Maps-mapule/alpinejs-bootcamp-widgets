c0ac2f825a672c63938b7cb4a142aa179d6b2c90
Maps-mapule <mapulemaluleke23@gmail.com>
2024-09-02 14:23:34 +0200

    Second Commit

--- greet.js
+++ greet.js
@@ -2,3 +2,11 @@ function greet(neighbourName) {
     return "Hello, " + neighbourName;
   }
    console.log(greet('Bob'));
+
+
+   document.addEventListener('DOMContentLoaded', (event) => {
+    const greetingElement = document.getElementById('greeting');
+    if (greetingElement) {
+      greetingElement.textContent = greet('Bob');
+    }
+  });
