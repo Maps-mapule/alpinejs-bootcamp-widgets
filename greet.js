a90998c58fb020bc7ffd775885f0f4b063a478dc
Maps-mapule <mapulemaluleke23@gmail.com>
2024-09-02 14:38:23 +0200

    Third Commit

--- greet.js
+++ greet.js
@@ -3,10 +3,27 @@ function greet(neighbourName) {
   }
    console.log(greet('Bob'));
 
+function transportFee(shift) {
+    switch(shift) {
+        case 'morning':
+            return 'R20';
+        case 'afternoon':
+            return 'R10';
+        case 'nightshift':
+            return 'free';
+      }
+}
 
-   document.addEventListener('DOMContentLoaded', (event) => {
+
+document.addEventListener('DOMContentLoaded', (event) => {
     const greetingElement = document.getElementById('greeting');
     if (greetingElement) {
       greetingElement.textContent = greet('Bob');
     }
+
+    const shiftElement = document.getElementById('shift-rate');
+    if (shiftElement) {
+      shiftElement.textContent = getShiftRate('morning'); // Replace 'morning' with the shift you want to test
+    }
   });
+  
